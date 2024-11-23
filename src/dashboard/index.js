e20dc77e5375554656fe3d1378c73a7351c054c6
Adrian Javier <javier.adrian.kld@gmail.com>
2024-11-23 22:45:22 +0800

    fix form submitting when closed and make statements responsive

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -3,7 +3,7 @@ var declareForm = function () {
         message:
             "<form id='frmDeclare' class='p-4' action='#' method='POST'>" +
                 '<div class="flex justify-start">' +
-                    '<button onclick="hideDeclareForm()" class="rounded-full bg-white relative flex max-w-xs items-center text-sm text-gray-400">' +
+                    '<button type="button" onclick="hideDeclareForm()" class="rounded-full bg-white relative flex max-w-xs items-center text-sm text-gray-400">' +
                         '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">' +
                             '<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />' +
                         '</svg>' +
@@ -93,7 +93,7 @@ var item = function(id, amount, description, type, timestamp) {
     return `
     <li class="flex justify-between gap-x-6 py-5" data-id="` + id.toString() + `">
         <div class="flex min-w-0 gap-x-6">
-            <div class="min-w-0 flex-auto pt-2 w-32">
+            <div class="min-w-0 flex-auto pt-2 w-16 sm:w-32">
                 <p class="text-xl font-semibold text-` + color + `-500 text-right">` + (amount/100).toFixed(2) + `</p>
             </div>
             <div class="min-w-0 flex-auto">
@@ -101,16 +101,16 @@ var item = function(id, amount, description, type, timestamp) {
                 <p class="mt-1 truncate text-xs/5 text-gray-500">` + dateString + `</p>
             </div>
         </div>
-        <div class="ml-4 mr-4 flex gap-8 justify-end md:ml-6 md:mr-6 relative">
+        <div class="ml-4 mr-4 flex flex-wrap gap-4 sm:gap-8 justify-end md:ml-6 md:mr-6 relative">
             <button onclick="" class="rounded-full bg-white relative flex max-w-xs items-center text-sm">
-                <svg class="size-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                <svg class="size-4 sm:size-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                      stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"/>
                 </svg>
             </button>
             <button onclick="" class="rounded-full bg-white relative flex max-w-xs items-center text-sm">
-                <svg class="size-6 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                <svg class="size-4 sm:size-6 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                      stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/>
@@ -158,11 +158,11 @@ var showDeclareForm = function () {
 
         $.post('../scripts/php/statement.php', declareObj, function (data) {
             console.log(data.msg)
+            getStatements()
         }, 'json')
 
         $('#frmDeclare').unblock();
         hideDeclareForm()
-        getStatements()
     })
 }
 
