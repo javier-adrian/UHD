1973af5d04ef4f939e833cd2f825885ed3d338e9
Adrian Javier <javier.adrian.kld@gmail.com>
2024-11-23 23:14:42 +0800

    fix edit and delete buttons scaling

--- src/dashboard/index.js
+++ src/dashboard/index.js
@@ -102,14 +102,14 @@ var item = function(id, amount, description, type, timestamp) {
             </div>
         </div>
         <div class="ml-4 mr-4 flex flex-wrap gap-4 sm:gap-8 justify-end md:ml-6 md:mr-6 relative">
-            <button onclick="" class="rounded-full bg-white relative flex max-w-xs items-center text-sm">
+            <button onclick="" class="basis-full sm:basis-0 rounded-full bg-white relative flex max-w-xs items-center text-sm">
                 <svg class="size-4 sm:size-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                      stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"/>
                 </svg>
             </button>
-            <button onclick="" class="rounded-full bg-white relative flex max-w-xs items-center text-sm">
+            <button onclick="" class="basis-full sm:basis-0 rounded-full bg-white relative flex max-w-xs items-center text-sm">
                 <svg class="size-4 sm:size-6 text-red-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                      stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
